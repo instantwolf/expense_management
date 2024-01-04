a57b84833dea5eb9435447310846b7f5f7f9a9de
crimsonshade <hueblerik@gmail.com>
2024-01-04 22:31:08 +0100

    add: Dropdown functionality

--- app/src/main/java/com/example/expensemanager/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/expensemanager/ui/home/HomeFragment.java
@@ -4,29 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.expensemanager.databinding.FragmentHomeBinding;
 
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        return binding.getRoot();
+    }
 
-        //final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initSpinners();
+    }
+
+    private void initSpinners() {
+        List<String> timeIntervalData = homeViewModel.getTimeIntervalData();
+        List<String> expenseCategoryData = homeViewModel.getExpenseCategoryData();
+
+        Spinner timeIntervalSpinner = binding.timeInterval;
+        Spinner expenseCategorySpinner = binding.expenseCategory;
+
+        ArrayAdapter<String> timerIntervalAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, timeIntervalData);
+        ArrayAdapter<String> expenseCategoryAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, expenseCategoryData);
+
+        timerIntervalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        expenseCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        timeIntervalSpinner.setAdapter(timerIntervalAdapter);
+        expenseCategorySpinner.setAdapter(expenseCategoryAdapter);
     }
 
     @Override
--- app/src/main/java/com/example/expensemanager/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/expensemanager/ui/home/HomeViewModel.java
@@ -4,14 +4,40 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> mText = new MutableLiveData<>();
+
+    // Define the time interval data
+    public final List<String> timeIntervalData = new ArrayList<>();
+
+    // Define the expense Category data
+    public final List<String> expenseCategoryData = new ArrayList<>();
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
+        // Initialize the time interval data
+        timeIntervalData.add("1 hour");
+        timeIntervalData.add("10 hour");
+        timeIntervalData.add("24 hour");
+        timeIntervalData.add("48 hour");
+
+        // Initialize the time interval data
+        expenseCategoryData.add("Food");
+        expenseCategoryData.add("Rent");
+        expenseCategoryData.add("Entertainment");
+        expenseCategoryData.add("Work");
+        expenseCategoryData.add("Hobby");
+        expenseCategoryData.add("Other");
+
         mText.setValue("This is the home/dashboard fragment");
     }
+
+    public List<String> getTimeIntervalData() { return timeIntervalData; }
+
+    public List<String> getExpenseCategoryData() { return expenseCategoryData; }
 
     public LiveData<String> getText() {
         return mText;
