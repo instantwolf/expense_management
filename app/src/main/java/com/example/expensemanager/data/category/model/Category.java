13a2e616fbc006c997e357700253bbd40acc37c9
crimsonshade <hueblerik@gmail.com>
2024-01-26 13:51:54 +0100

    fix: Category bug
    just commented out the not working method

--- app/src/main/java/com/example/expensemanager/data/category/model/Category.java
+++ app/src/main/java/com/example/expensemanager/data/category/model/Category.java
@@ -23,9 +23,9 @@ public class Category {
 
     public String getName() { return this.name; }
 
-    public int getExpenseCount() {
-        return this.expenses.size();
-    }
+//    public int getExpenseCount() {
+//        return this.expenses.size();
+//    }
   
     public boolean matches(String name){
         return getEqualityString(this.name).equals(getEqualityString(name));
